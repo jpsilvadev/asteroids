1f330329daa19b72abd02df463a20a8f0806e55d
jpsilvadev <jpsjoaosilva98@outlook.com>
2025-01-31 15:09:52 +0000

    add player sprite and movement

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from constants import *
 import pygame
+from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # allow closing winddow
@@ -12,8 +18,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
